2efd8c13ec3530b17cdc2abfee6f6ffedec6449e
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-12-08 10:12:01 +0000

    some tests passing

---
+++ retreat.py
@@ -0,0 +1,68 @@
+__metaclass__ = type
+
+import numpy as np
+from random import choice
+
+
+class Cell:
+    def __init__(self, state=None):
+        if state is not None:
+            self.state = state
+        else:
+            self.state = choice((True, False))
+
+    def __eq__(self, other):
+        if type(other) == bool:
+            return self.state == other
+        else:
+            return self.state == other.state
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        if self.state:
+            return 'o'
+        else:
+            return ' '
+
+class Grid:
+    def __init__(self, dim, state=None):
+        self.dim = dim
+        self.grid = []
+        for i in range(self.dim):
+            self.grid.append([])
+            for j in range(self.dim):
+                self.grid[i].append(Cell(state))
+
+    def __str__(self):
+        line_str = lambda l: ' '.join(str(c) for c in l)
+        return '\n'.join(line_str(x) for x in self.grid)
+
+    def filter_out(self, all_neigh):
+        res = []
+        valid = lambda x: (x >= 0) and (x < self.dim)
+        for neigh in all_neigh:
+            if valid(neigh[0]) and valid(neigh[1]):
+                res.append(neigh)
+
+        return res
+
+    def neighbours(self, x, y):
+        """Return list of neighbour cells
+        """
+        upper = [(x-1, j) for j in range(y-1, y+2)]
+        lower = [(x+1, j) for j in range(y-1, y+2)]
+        l_r = [(x, y-1), (x, y+1)]
+        all_neighbours = upper + lower + l_r
+        return self.filter_out(all_neighbours)
+
+    def __getitem__(self, item):
+        return self.grid[item]
+
+
+if __name__ == '__main__':
+    g = Grid(10)
+    print(g)
+    g2 = Grid(10, False)
+    print(g2)
---
+++ test_retreat.py
@@ -0,0 +1,23 @@
+import unittest
+import retreat
+
+
+class TestLife(unittest.TestCase):
+    def test_empty_grid(self):
+        grid = retreat.Grid(4, state=False)
+        self.assertEqual(grid[0][0], retreat.Cell(False))
+
+    def test_simple_grid(self):
+        grid = retreat.Grid(4, state=True)
+        grid[0][1] = retreat.Cell(False)
+        self.assertEqual(grid[0][1], False)
+
+    def test_neighbour_middle(self):
+        grid = retreat.Grid(3, state=True)
+        neigh_middle = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)]
+        self.assertEqual(sorted(grid.neighbours(1, 1)), sorted(neigh_middle))
+
+    def test_neighbour_corner(self):
+        grid = retreat.Grid(3, state=True)
+        neigh_zero = [(0, 1), (1, 0), (1, 1)]
+        self.assertEqual(sorted(grid.neighbours(0, 0)), neigh_zero)
